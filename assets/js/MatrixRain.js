afc04f6de8f1933aabe260e5e376312f3fd38a63
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-02-21 17:42:12 +0100

    Giga js Update

---
+++ assets/js/MatrixRain.js
@@ -0,0 +1,49 @@
+﻿// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = 'アカサタナハマヤャラワガザダバパイキシチニヒミリヰギジヂビピウクスツヌフムユュルグズヅブプエケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 17,
+    columns = canvas.width / fontSize;
+
+//Random Method
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Setting up the drops
+var drops = [];
+for (var i = columns; i > 0 ; i--) {
+  drops[i] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+  ctx.fillStyle = 'rgba(0, 0, 0, .15)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  for (var i = 0; i < drops.length; i++) {
+    //pick a random character
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    
+    ctx.fillStyle = '#0f0'; //This is the text color
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 20 + getRandomInt(4); //loop height variations
+    }
+  }
+  
+}
+
+// Loop the animation
+setInterval(draw, 33);
---
+++ assets/js/TerminalText.js
@@ -0,0 +1,42 @@
+consoleText(['Thomas Depraz-Depland'], 'text',  ['white'])
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];//set to white if undefined
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === words[0].length + 1 && waiting === false) 
+    {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x; 
+      }, 1000)
+    } 
+    else if (waiting === false) 
+    {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120) //Character writing speed
+  
+  //Underscore Blink
+  window.setInterval(function() {
+    if (visible === true) 
+    {
+      con.className = 'console-underscore hidden'
+      visible = false;
+    }   
+    else 
+    {
+      con.className = 'console-underscore'
+      visible = true;
+    }
+  }, 400)
+}
